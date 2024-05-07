f0f12bdd72718e375782c1a28a5e830d3dde875a
hayanbeigh0 <95053707+hayanbeigh0@users.noreply.github.com>
2024-05-08 00:37:38 +0530

    OPEN: Disabled apis

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const hpp = require("hpp");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
-// const userRouter = require("./routes/userRoutes");
-// const reviewRouter = require("./routes/reviewRoutes");
-// const organisationRouter = require("./routes/organisationRoutes");
-// const vehicleRouter = require("./routes/vehicleRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const organisationRouter = require("./routes/organisationRoutes");
+const vehicleRouter = require("./routes/vehicleRoutes");
 const uploadRouter = require('./routes/uploadRoutes');
 
 const app = express();
@@ -71,11 +71,11 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/v1/tours", tourRouter);
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/reviews", reviewRouter);
-// app.use("/api/v1/organisations", organisationRouter);
-// app.use("/api/v1/buses", vehicleRouter);
-// app.use('/api/v1/uploads', uploadRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/organisations", organisationRouter);
+app.use("/api/v1/buses", vehicleRouter);
+app.use('/api/v1/uploads', uploadRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find the ${req.originalUrl} on this server!`, 404));
