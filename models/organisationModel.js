9b6d321fe89f64117a5d39a6f672966f2b99cda3
hayanbeigh <hayanbeigh117@gmail.com>
2024-05-10 15:29:54 +0530

    remove: Commented code

--- models/organisationModel.js
+++ models/organisationModel.js
@@ -56,76 +56,10 @@ const organisationSchema = new mongoose.Schema(
 
 organisationSchema.index({ name: 1, buses: 1 });
 
-organisationSchema.post(
-  "remove",
-  { document: true, query: false },
-  async function (doc) {
-    if (doc) {
-      console.log(doc);
-      const orgId = doc._id;
-      console.log(`Removing organisation with ID: ${orgId}`);
-
-      // Update all users that reference this organisation
-      await User.updateMany({}, { $pull: { organisations: orgId } });
-    }
-  }
-);
-
 organisationSchema.pre(/^find/, function (next) {
   this.find({ active: { $ne: false } }); // This will not return any organisation which has "active" set to false.
   next();
 });
-
-// organisationSchema.index({ tour: 1, user: 1 }, { unique: true });
-
-// organisationSchema.pre(/^find/, function (next) {
-//   this.populate({
-//     path: "user",
-//     select: "name photo",
-//   });
-//   next();
-// });
-
-// organisationSchema.statics.calcAverageRatings = async function (tourId) {
-//   const stats = await this.aggregate([
-//     {
-//       $match: { tour: tourId },
-//     },
-//     {
-//       $group: {
-//         _id: "$tour",
-//         nRating: { $sum: 1 },
-//         avgRating: { $avg: "$rating" },
-//       },
-//     },
-//   ]);
-
-//   if (stats.length > 0) {
-//     await Tour.findByIdAndUpdate(tourId, {
-//       ratingsQuantity: stats[0].nRating,
-//       ratingsAverage: stats[0].avgRating,
-//     });
-//   } else {
-//     await Tour.findByIdAndUpdate(tourId, {
-//       ratingsQuantity: 0,
-//       ratingsAverage: 4.5,
-//     });
-//   }
-// };
-
-// organisationSchema.post("save", function () {
-//   this.constructor.calcAverageRatings(this.tour);
-// });
-
-// organisationSchema.pre(/findOneAnd/, async function (next) {
-//   this.r = await this.findOne();
-//   next();
-// });
-
-// organisationSchema.post(/findOneAnd/, async function () {
-//   // this.findOne() does not work here as the query has already executed.
-//   await this.r.constructor.calcAverageRatings(this.r.tour);
-// });
 
 const Organisation = mongoose.model("Organisation", organisationSchema);
 
